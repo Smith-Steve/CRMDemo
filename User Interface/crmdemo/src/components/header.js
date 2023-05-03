d8a68ebf99d886ad375d1bbe67fa045fe67aa182
Smith-Steve <Steve.A.Dore@outlook.com>
2023-05-02 21:55:53 -0700

    currently displaying minor changes made to header app

--- User Interface/crmdemo/src/components/header.js
+++ User Interface/crmdemo/src/components/header.js
@@ -8,8 +8,8 @@ export default function Header(){
         <div className='header yblue'>
             <h3 className='white'>Demo CRM</h3>
         </div>
-        <nav className='navbar navbar-expand-sm bg-light navbar-dark'>
-            <ul className='navbar-nav'>
+        <nav className='navbar navbar-expand-sm bg-light yblue'>
+            <ul className='navbar-nav yblue'>
                 <li className='nav-item m-1'>
                     <NavLink className="btn btn-light btn-outline-primary" to="/home">
                         Home
@@ -37,13 +37,12 @@ export default function Header(){
                 </li>
             </ul>
         </nav>
-        <Routes>
-            <Route path="/home" Component={Home}/>
+        {/* <Routes>
             <Route path="/Customers" Component={Customers}/>
             <Route path="/Contacts" Component={Contacts}/>
             <Route path="/Flights" Component={Flights}/>
             <Route path="/Emails" Component={Emails}/>
-        </Routes>
+        </Routes> */}
         </React.Fragment>
     )
 }
