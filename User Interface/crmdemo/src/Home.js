d9b19408736674c8e9f3a812a533c78aec431fa6
Smith-Steve <Steve.A.Dore@outlook.com>
2023-05-04 20:25:15 -0700

    rearrangement of components.

--- User Interface/crmdemo/src/Home.js
+++ User Interface/crmdemo/src/Home.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import Sidebar from './components/sidebar';
 import {BrowserRouter, Route, Switch, Routes} from 'react-router-dom'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Sidebar></Sidebar>
-    </BrowserRouter>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.activeComponent = this.setActiveComponent.bind(this)
+  }
+
+  setActiveComponent(selectComponent) {
+    this.setState({ activeComponent: selectComponent});
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Sidebar></Sidebar>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
+export default Home;
