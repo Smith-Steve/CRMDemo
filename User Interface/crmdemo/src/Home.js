5769125cf20fcb5787f16f11898039bd07f19c10
Smith-Steve <Steve.A.Dore@outlook.com>
2023-05-04 19:34:31 -0700

    Rearranged Components; 1.) Removed Headbar 2.) fixed sidebar along traditional

--- User Interface/crmdemo/src/Home.js
+++ User Interface/crmdemo/src/Home.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
-import Header from './components/header'
 import Sidebar from './components/sidebar';
 import {BrowserRouter, Route, Switch, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
-      <Header></Header>
       <Sidebar></Sidebar>
     </BrowserRouter>
   );
--- User Interface/crmdemo/src/components/sidebar.js
+++ User Interface/crmdemo/src/components/sidebar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Sidebar(){
     return (
-        <div className='side-bar lg rounded-corners nyellow'>
-
+        <div className='side-bar lg rounded-corners'>
+            <React.Fragment>
+                <div className='col'>
+                    <div className='sidenav'>
+                        <a href="">Home</a>
+                        <a href='/Customers'>Customers</a>
+                        <a href='/Contacts'>Contacts</a>
+                        <a href='/Flights'>Flights</a>
+                        <a href="/Emails">Emails</a>
+                    </div>
+                </div>
+            </React.Fragment>
         </div>
     )
 }
