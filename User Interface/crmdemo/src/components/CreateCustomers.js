7524deb7307ab8a18c4232fbc8e398f58fee5646
Smith-Steve <Steve.A.Dore@outlook.com>
2023-05-09 22:23:57 -0700

    almost complete customer entry page

--- User Interface/crmdemo/src/components/CreateCustomers.js
+++ User Interface/crmdemo/src/components/CreateCustomers.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import ApiVariables from '../Library/API_URLS'
+import APICalls from '../Library/API_Calls'
 
 export default class CreateCustomer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { CustomerName: '', CustomerStreet1: '', CustomerCity: '', CustomerState: '', CustomerVertical: ''}
+        this.handleChange = this.handleChange.bind()
+        this.state = { CustomerName: '', CustomerStreet1: '', CustomerCity: '', CustomerState: '', CustomerVertical: '', ComponentApiString: 'CustomerAPI'}
+    }
+
+    createNewCustomer(){
+        this.setState({
+            CustomerName: '',
+            CustomerStreet1: '',
+            CustomerCity: '',
+            CustomerState: '',
+            CustomerVertical: ''
+        })
+    }
+
+    createNewCustomerClick(){
+        fetch(ApiVariables.variables.API_URL+this.state.ComponentApiString,
+            {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    CustomerName: this.state.CustomerName,
+                    CustomerStreet1: this.state.CustomerStreet1,
+                    CustomerCity: this.state.CustomerCity,
+                    CustomerState: this.state.CustomerState,
+                    CustomerVertical: this.state.CustomerVertical
+                })
+            }).then(
+                response => response.json()
+            ).then((returnedResponse) => {
+                if(returnedResponse){
+                    alert('Submitted')
+                    this.clearForm()
+                } else {
+                    alert('Not Submitted')
+                }
+            }).catch(error => {
+                if (error) throw error;
+            })
+    }
+
+    handleChange(event){
+        const name = event.target.name;
+        this.setState({ [name]: event.target.value})
     }
 
     clearForm() {
@@ -13,9 +60,41 @@ export default class CreateCustomer extends React.Component {
     render() {
         return(
             <div className='CreateCustomer'>
-                        <div>
-                            <h1 className='green'>Add Customer</h1>
+                <div className='center'>
+                    <h1 className='green'>Add Customer</h1>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <div className='form-container rounded-corners lg'>
+                            <form>
+                                <div className='input-row'>
+                                    <label>Customer Name</label>
+                                    <input type="text" name="CustomerName" value={this.state.CustomerName} onChange={this.handleChange} required></input>
+                                </div>
+                                <div className='input-row'>
+                                    <label>Customer Street 1</label>
+                                    <input type="text" name="Customer Street 1" value={this.state.CustomerStreet1} onChange={this.handleChange} required></input>
+                                </div>
+                                <div className='input-row'>
+                                    <label>Customer City</label>
+                                    <input type="text" name="CustomerCity" value={this.state.CustomerCity} onChange={this.handleChange} required></input>
+                                </div>
+                                <div className='input-row'>
+                                    <label>Customer State</label>
+                                    <input type="text" name="CustomerState" value={this.state.CustomerState} onChange={this.handleChange} required></input>
+                                </div>
+                                <div className='input-row'>
+                                    <label>Customer Vertical</label>
+                                    <input type="text" name="CustomerVertical" value={this.state.CustomerVertical} onChange={this.handleChange} required></input>
+                                </div>
+                                <div className='align-right'>
+                                    <button onSubmit={this.createNewCustomerClick}>Enter Customer</button>
+                                </div>
+                            </form>
                         </div>
+                    </div>
+
+                </div>
             </div>
         )
     }
