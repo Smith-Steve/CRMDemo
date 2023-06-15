3feb9a74d8e477c3d90103b635977e151735e9a4
Smith-Steve <Steve.A.Dore@outlook.com>
2023-06-15 16:00:47 -0700

    small change.

--- User Interface/crmdemo/src/components/Flights/EmailConfiguration.jsx
+++ User Interface/crmdemo/src/components/Flights/EmailConfiguration.jsx
@@ -51,6 +51,11 @@ export default class EmailConfiguration extends React.Component {
     }
 
     render(){
+        //This form is going to have to be changed so that this form will be inaccessible if we
+        //attempt to update an email that has already been sent.
+
+        //Functionality will also have to exist to copy emails/flights etc.. so they
+        //can be used again.
         return(
             <div className={this.state.Component}>
                 <div className='center'>
@@ -60,11 +65,9 @@ export default class EmailConfiguration extends React.Component {
                             <h1 className='green'>Update Email</h1>
                         </React.Fragment>)
                         :
-                        (
-                            <React.Fragment>
-                                <h1 className='green'>Add Email</h1>
-                            </React.Fragment>
-                        )
+                        (<React.Fragment>
+                            <h1 className='green'>Add Email</h1>
+                        </React.Fragment>)
                     }
                 </div>
                 <div className='row'>
@@ -72,23 +75,68 @@ export default class EmailConfiguration extends React.Component {
                         <div className='form-container rounded-corners lg'>
                             <form onSubmit={this.handleSubmit}>
                                 <div className='input-row'>
-                                    <label>Email Name</label>
+                                    {
+                                        this.props.activeEmail != null ?
+                                        (<React.Fragment>
+                                            <label>Update Email Name</label>
+                                        </React.Fragment>)
+                                        :
+                                        (<React.Fragment>
+                                            <label>Email Name</label>
+                                        </React.Fragment>)
+                                    }
                                     <input className='rounded-corners' type='text' name='EmailName' value={this.state.EmailName} onChange={this.handleChange} required></input>
                                 </div>
                                 <div className='input-row'>
-                                    <label>Email Subject Title</label>
+                                    {
+                                        this.props.activeEmail != null ?
+                                        (<React.Fragment>
+                                            <label>Update Subject Title</label>
+                                        </React.Fragment>)
+                                        :
+                                        (<React.Fragment>
+                                            <label>Email Subject Title</label>
+                                        </React.Fragment>)
+                                    }
                                     <input className='rounded-corners' type='text' name='EmailSubjectTitle' value={this.state.EmailSubjectTitle} onChange={this.handleChange} required></input>
                                 </div>
                                 <div className='input-row'>
-                                    <label>Email Body</label>
+                                    {
+                                        this.props.activeEmail != null ?
+                                        (<React.Fragment>
+                                            <label>Update Email Body</label>
+                                        </React.Fragment>)
+                                        :
+                                        (<React.Fragment>
+                                            <label>Email Body</label>
+                                        </React.Fragment>)
+                                    }
                                     <textarea className='rounded-corners' type='text' rows="6" name='EmailBody' value={this.state.EmailBody} onChange={this.handleChange} required></textarea>
                                 </div>
                                 <div className='input-row'>
-                                    <label>Send On</label>
+                                    {
+                                        this.props.ActiveEmail != null ?
+                                        (<React.Fragment>
+                                            <label>Update Send On</label>
+                                        </React.Fragment>)
+                                        :
+                                        (<React.Fragment>
+                                            <label>Send On</label>
+                                        </React.Fragment>)
+                                    }
                                     <input className='rounded-corners' type='datetime-local' name='SendOn' value={this.state.SendOn} onChange={this.handleChange} required></input>
                                 </div>
                                 <div className='input-row'>
-                                    <label>Email Number In Sequence</label>
+                                    {
+                                        this.props.activeEmail != null ?
+                                        (<React.Fragment>
+                                            <label>Change Email Number In Sequence</label>
+                                        </React.Fragment>)
+                                        :
+                                        (<React.Fragment>
+                                            <label>Email Number In Sequence</label>
+                                        </React.Fragment>)
+                                    }
                                     <input className='rounded-corners' type='number' min="1" max="10" name='EmailNumberInSequence' value={this.state.EmailNumberInSequence} onChange={this.handleChange} required></input>
                                 </div>
                                 <div className='align-right'>
